c60f5290b04f9fa36cbae281a866d1f61665d85e
khoin090521 <nguyenquangkhoi07@gmail.com>
2024-06-14 00:06:28 +0700

    initial commit

---
+++ PetBE/src/main/java/com/example/petbe/PetBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.petbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetBeApplication.class, args);
+    }
+
+}
---
+++ PetBE/src/test/java/com/example/petbe/PetBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.petbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
